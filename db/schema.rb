f5745846d57e5762a16ed7f56415b395259935eb
GeneTasico <gtasico@rrc.com>
2025-04-03 20:24:42 -0500

    Set up database and migrated it.

---
+++ db/schema.rb
@@ -0,0 +1,72 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_04_04_012301) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "collectibles", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "price"
+    t.integer "stock_quantity"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_collectibles_on_category_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "customer_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "price"
+    t.decimal "subtotal"
+    t.integer "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "ordered_collectibles", force: :cascade do |t|
+    t.integer "order_item_id", null: false
+    t.integer "collectible_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["collectible_id"], name: "index_ordered_collectibles_on_collectible_id"
+    t.index ["order_item_id"], name: "index_ordered_collectibles_on_order_item_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "total_amount"
+    t.datetime "order_date"
+    t.string "status"
+    t.decimal "tax"
+    t.integer "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  add_foreign_key "collectibles", "categories"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "ordered_collectibles", "collectibles"
+  add_foreign_key "ordered_collectibles", "order_items"
+  add_foreign_key "orders", "customers"
+end
