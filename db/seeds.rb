f76a830d9ab8bb16113e876a81c1ba46d0ae7a47
GeneTasico <gtasico@rrc.com>
2025-04-03 21:09:46 -0500

    1.5 Create and maintain a list of product categories and assign categories to new or existing products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# Creating my categories
+categories = ['Jerseys', "Signed Memorabilia", "Equipment", "Trading Cards"]
+categories.each do |category_name|
+  Category.create(name:category_name)
+end
